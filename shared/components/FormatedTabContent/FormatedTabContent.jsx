5d4f1c634b906499524b9bb4abe53aaee412ef6a
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-02-27 10:51:02 +0200

    Create FormatedTabContent and styles

--- shared/components/FormatedTabContent/FormatedTabContent.jsx
+++ shared/components/FormatedTabContent/FormatedTabContent.jsx
@@ -1,26 +1,58 @@
-export default function FormattedText({ content }) {
+import styles from "./FormatedTabContent.module.css";
+
+export default function FormattedText({
+  content,
+  className,
+  paddingContent,
+  tab,
+  activeTab,
+}) {
   const formatText = (input) => {
     return input.split("&").map((paragraph, index) => {
       if (paragraph.includes("*")) {
+        const parts = paragraph.split("*").map((part) => part.trim());
+        const title = parts[0];
+        const listItems = parts.slice(1);
+
         return (
-          <ul key={paragraph + index}>
-            {paragraph.split("*").map((line, i) => {
-              return <li key={i}>{line}</li>;
-            })}
-          </ul>
+          <div key={index}>
+            {title && <p className={styles.paragraph}>{title}</p>}
+            <ul className={styles.list}>
+              {listItems.map((line, i) => (
+                <li key={i} className={styles.listItem}>
+                  {line}
+                </li>
+              ))}
+            </ul>
+          </div>
         );
       }
 
       const paragraphWithBreaks = paragraph.split("&&").map((line, i) => (
-        <span key={line + i}>
+        <span key={i}>
           {line}
           {i === paragraph.split("&&").length - 1 && <br />}
         </span>
       ));
 
-      return <p key={index}>{paragraphWithBreaks}</p>;
+      return (
+        <p key={index} className={styles.paragraph}>
+          {paragraphWithBreaks}
+        </p>
+      );
     });
   };
 
-  return <div>{formatText(content)}</div>;
+  return (
+    <div
+      className={className}
+      style={{
+        paddingLeft: `${paddingContent}px`,
+        paddingRight: `${paddingContent * 0.4}px`,
+        marginBottom: activeTab === tab ? "28px" : "0",
+      }}
+    >
+      {formatText(content)}
+    </div>
+  );
 }
