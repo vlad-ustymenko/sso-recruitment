d450109cb267eaa62a2d07e3178bac63af0d7ccc
Vladyslav Ustymenko <vlad.ustimenko@gmail.com>
2025-03-25 19:56:00 +0200

    edit config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dffb9mawi/image/upload/**",
+      },
+    ],
+  },
   async headers() {
     return [
       {
